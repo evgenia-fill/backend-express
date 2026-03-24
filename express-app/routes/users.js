00779d6186fde906d1bb0503929f4aa4175e67f7
evgeniavolkova <evgeniafil04@gmail.com>
2026-03-24 19:02:17 +0500

    task 6

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,27 +1,37 @@
-
 const express = require('express');
 const router = express.Router();
 
 // Массив для хранения пользователей
 const users = [
-  { id: 1, name: "Степа" },
-  { id: 2, name: "Женя" },
+    {id: 1, name: "Степа"},
+    {id: 2, name: "Женя"},
 ];
 
 
-
-router.get('/', function(req, res, next) {
-  res.json({
-    items: users
-  });
+router.get('/', function (req, res, next) {
+    res.json({
+        items: users
+    });
 });
 
 
-router.post('/', function(req, res, next) {
-  const newUser = req.body;
-  newUser.id = users.length > 0 ? users[users.length - 1].id + 1 : 1;
-  users.push(newUser);
-  res.status(201).json(newUser);
+router.post('/', function (req, res, next) {
+    const newUser = req.body;
+    newUser.id = users.length > 0 ? users[users.length - 1].id + 1 : 1;
+    users.push(newUser);
+    res.status(201).json(newUser);
 });
 
 module.exports = router;
+
+
+router.get('/:id', function (req, res, next) {
+    const params = req.params;
+    const user = users.find(x => x.id === params.id);
+
+    if (!user) {
+        return res.status(404).send('Not Found');
+    }
+
+    res.json(user);
+})
